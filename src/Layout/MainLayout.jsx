a0d1ea7a3ddeb3895f9188d0f0df7a4cb21da3fe
fnhnafiz <nafsinsifan2k21@gmail.com>
2025-06-30 00:47:49 +0600

    first commit

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar";
+
+function MainLayout() {
+  return (
+    <div>
+      <Navbar />
+      <main className="">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default MainLayout;
